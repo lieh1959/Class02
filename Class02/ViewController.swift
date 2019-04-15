49a8ee414fdac9b1cf7f15919ffbec18ed619a19
lieh1959 <lieh1959@gmail.com>
2019-04-15 12:01:32 +0800

    0415-01

--- Class02/ViewController.swift
+++ Class02/ViewController.swift
@@ -13,6 +13,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let a = add(a:4,b:3)
+        
+        if (a>0){
+            print("a 值為：\( a ）")
+        }
+        else{
+            print("\")
+        }
+        
+        func add(a: Int, b : Int) -> Int {
+            return a+b
+        }
     }
 
 
